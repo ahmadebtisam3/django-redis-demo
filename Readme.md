387fe69ab5a6d94058c3e22f5492d1b5290f096c
Ibtisam Ahmad <ahmadebtisam3@gmail.com>
2021-09-30 17:20:37 +0500

    added users group and permitions

--- Readme.md
+++ Readme.md
@@ -1,8 +1,12 @@
 # Redis with django
 in this small demo we use redis to store ip of
-all the users who send request above than the define number of request in a mint and finnaly
-we block those ips from accessing our site . All of this logic is implemented in demo apps
-middleware
+all the users who send request . we define limits to each users to send request to our site if users 
+send request above than the defined number of request in a mint
+we block those ips from accessing our site . users are divided in to four groups first one are 
+unauthrized user they can access the site 5 times in a minut , second ones are golden group users 
+they can access 10 times/min third ones are silver they can access 8 times and last group types are 
+platinum they can access 6 times/min .All of this logic is implemented in demo apps
+middleware . 
 
 ## deployment
 ```bash
@@ -10,9 +14,8 @@ middleware
     sudo docker run -itp 6379:6379 --name run_reds redis 
     python -m venv django_install
     source ./django_install/bin/active
-    pip install django
-    pip install redis
     git clone https://github.com/ahmadebtisam3/django-redis-demo.git
     cd redisApp
+    pip install -r requirements.txt
     python manage.py runserver
 ```
--- redisDemo/requestAllowedMiddleware.py
+++ redisDemo/requestAllowedMiddleware.py
@@ -10,11 +10,27 @@ class RequestAllowed(object):
         return self.get_response(request)
 
     def process_view(self, request, view_func, *view_args, **view_kargs):
+        allowed = True
+        try:
+            if not request.user.is_authenticated:
+                allowed = self._unauthorized_user_permitions(request, 5)
+            elif request.user.groups.filter(name="golden"):
+                allowed = self._unauthorized_user_permitions(request, 10)
+            elif request.user.groups.filter(name="silver"):
+                allowed = self._unauthorized_user_permitions(request, 8)
+            elif request.user.groups.filter(name="platinium"):
+                allowed = self._unauthorized_user_permitions(request, 6)
+        except Exception as e:
+            print(" *********** error occured  ", e)
+        if not allowed:
+            return HttpResponse(status=400)
+
+    def _unauthorized_user_permitions(self, request, tim):
         conn = redis.Redis(host="127.0.0.1", port=6379)
         ip = request.META['REMOTE_ADDR']
         times = conn.get(ip)
         if times:
-            if int(times) < 5:
+            if int(times) < tim:
                 conn.incr(ip)
             else:
                 conn.sadd("blockip", ip)
@@ -22,4 +38,5 @@ class RequestAllowed(object):
             conn.set(ip, 0)
             conn.expire(ip, 60)
         if bytes(ip, 'utf-8') in conn.smembers("blockip"):
-            return HttpResponse(status=400)
+            return False
+        return True
