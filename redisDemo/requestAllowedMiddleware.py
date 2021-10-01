d1b778c20484b6941ee6753ac61197091f90ff94
Ibtisam Ahmad <ahmadebtisam3@gmail.com>
2021-10-01 14:31:35 +0500

    improve code structure

--- redisDemo/requestAllowedMiddleware.py
+++ redisDemo/requestAllowedMiddleware.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 
 class RequestAllowed(object):
+    PERMISSIONS = {"golden": 10, "silver": 8, "platinium": 6, "user": 4}
+    BLOCKLIST = "blockip"
+    BLOCKTIME = 100
+    conn = redis.Redis(host="127.0.0.1", port=6379)
+
     def __init__(self, response):
         self.get_response = response
 
@@ -10,33 +15,34 @@ class RequestAllowed(object):
         return self.get_response(request)
 
     def process_view(self, request, view_func, *view_args, **view_kargs):
-        allowed = True
         try:
-            if not request.user.is_authenticated:
-                allowed = self._unauthorized_user_permitions(request, 5)
-            elif request.user.groups.filter(name="golden"):
-                allowed = self._unauthorized_user_permitions(request, 10)
-            elif request.user.groups.filter(name="silver"):
-                allowed = self._unauthorized_user_permitions(request, 8)
-            elif request.user.groups.filter(name="platinium"):
-                allowed = self._unauthorized_user_permitions(request, 6)
+            if request.user.is_authenticated:
+                for key, time in self.PERMISSIONS.items():
+                    if request.user.groups.filter(name=key):
+                        return self.request_allowed(self.conn, request, time)
+            return self.request_allowed(self.conn, request,
+                                        self.PERMISSIONS['user'])
         except Exception as e:
             print(" *********** error occured  ", e)
-        if not allowed:
-            return HttpResponse(status=400)
 
-    def _unauthorized_user_permitions(self, request, tim):
-        conn = redis.Redis(host="127.0.0.1", port=6379)
+    def request_allowed(self, conn, request, tim):
         ip = request.META['REMOTE_ADDR']
         times = conn.get(ip)
         if times:
             if int(times) < tim:
                 conn.incr(ip)
             else:
-                conn.sadd("blockip", ip)
+                self.block_ip(ip, conn)
         else:
             conn.set(ip, 0)
             conn.expire(ip, 60)
         if bytes(ip, 'utf-8') in conn.smembers("blockip"):
-            return False
-        return True
+            return HttpResponse(status=400)
+        return None
+
+    def block_ip(self, ip, conn):
+        if conn.smembers("blockip"):
+            conn.sadd(self.BLOCKLIST, ip)
+        else:
+            conn.sadd(self.BLOCKLIST, ip)
+            conn.expire(self.BLOCKLIST, self.BLOCKTIME)
